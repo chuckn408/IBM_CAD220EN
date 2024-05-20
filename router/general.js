bc7121222fac59a67e776d13478f4821c3dc588e
chuckn408 <nelsoch@pm.me>
2024-05-20 02:08:15 -0400

    adding task 1

--- router/general.js
+++ router/general.js
@@ -12,13 +12,14 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(friends,null,4));
+    res.send(JSON.stringify(books,null,4));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+    const isbn = req.params.isbn;
+    res.send(books[isbn])
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
